5c63a40b12131fd17fef381f36f4cb3a0045c490
TeamSkibidi <skibiditolet553@gmail.com>
2025-03-15 12:40:48 +0700

    Update UIGame.cpp

--- UIGame.cpp
+++ UIGame.cpp
@@ -7,11 +7,11 @@
 #include "UIGame.h"
 #include "DashBoard.h"
 #include "Grid.h"
+#include "Game.h"
 
-
+// Measure game FPS
 void UIGame::checkFPS()
 {
-
 	this->window->setFramerateLimit(60);
 	double ending = clock.getElapsedTime().asSeconds();
 	double timeFPS = ending - starting;
@@ -34,23 +34,25 @@ void UIGame::initGrid()
 	this->grid = new GridGame(this->window);
 }
 
-UIGame::UIGame(sf::RenderWindow* window)
-{
+// Constructor and destructor Game UI
+UIGame::UIGame(sf::RenderWindow* window, Game_Functions& GF) : GameFunction(GF) {
 	this->window = window;
 	this->initDashBoard();
 	this->initGrid();
-}
+	this->update();
+	this->GameFunction = Game_Functions::GAMECLASSIC;
 
+}
 
 UIGame::~UIGame()
 {
 	delete this->dashboard;
 	delete this->grid;
+	
 	this->dashboard = nullptr;
 	this->grid = nullptr;
 
 }
-
 
 const bool UIGame::running() const
 {
@@ -58,51 +60,84 @@ const bool UIGame::running() const
 }
 
 
+void UIGame::updateWindow()
+{
+	this->view = sf::View(sf::FloatRect(0, 0, 600, 800));
+
+	if (this->event.type == sf::Event::Resized)
+	{
+		// Get size window new
+		float Update_width = static_cast<float>(this->event.size.width);
+		float Update_height = static_cast<float>(this->event.size.height);
+
+		// Get the new window scale
+		float Update_board = Update_width / Update_height;
+
+		// Get window scale
+		float board = 600.0f / 800.0f;
+
+		// Update size the new window
+		if (Update_board > board) {
+			this->view.setSize(static_cast <float>(800 * Update_board), static_cast <float>(800));
+		}
+		else {
+			this->view.setSize(static_cast <float>(600), static_cast <float>(600 / Update_board));
+		}
+		this->window->setView(view);
+	}
+
+}
+
 void UIGame::pollevent()
 {
 	if (!this->window) return;
-
-
-	this->view = sf::View(sf::FloatRect(0, 0, 600, 800));
 
 	while (this->window->pollEvent(event)) {
 		if (this->event.type == sf::Event::Closed) {
 			this->window->close();
 		}
-		else if (this->event.type == sf::Event::Resized) {
-			float Update_width = static_cast<float>(this->event.size.width);
-			float Update_height = static_cast<float>(this->event.size.height);
-
-			float Update_board = Update_width / Update_height;
-			float board = 600.0f / 800.0f;
-
-			if (Update_board > board) {
-				this->view.setSize(static_cast <float>(800 * Update_board), static_cast <float>(800));
-			}
-			else {
-				this->view.setSize(static_cast <float>(600), static_cast <float>(600 / Update_board));
-			}
-			this->window->setView(view);
+		// Check for window changes
+		this->updateWindow();
+		
+		// Detect mouse button click 
+		if (this->GameFunction == Game_Functions::GAMECLASSIC) {
+			this->dashboard->pressdFuncion(event, this->GameFunction);
+			std::cout << "dmm";
 		}
+		std::cout << "z";
+	}
+}
+
+void UIGame::updatePollevent()
+{
+	if (this->GameFunction == Game_Functions::HOME) {
+		this->GameFunction = Game_Functions::DELETEGAME;
 	}
 }
 
 void UIGame::update()
 {
 	this->pollevent();
+	this->updatePollevent();
 }
 
+
+// Render game UI including dashboard and grid
 void UIGame::render()
 {
 	checkFPS();
 	if (!this->window)
 		return;
 
-	/*this->window->clear(sf::Color(245, 243, 227));*/
-	this->dashboard->render();
-	this->grid->RenderGridGame();
-
+	if (this->GameFunction == Game_Functions::HOME) {
+		this->mainMenu->render();
+	}
+	else if (this->GameFunction == Game_Functions::GAMECLASSIC) {
+		this->dashboard->render();
+		this->grid->RenderGridGame();
+	}
 }
+
 
 
 
